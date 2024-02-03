34bf9239e55fc59ffab6276b8be719d847fe742a
Brambs <gabriel128.br@gmail.com>
2024-02-03 14:21:25 -0300

    Detail Page start

--- src/pages/ExerciseDetail.jsx
+++ src/pages/ExerciseDetail.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+import { Detail } from '../components/Detail'
+import { ExerciseVideos } from '../components/ExerciseVideos'
+
 
 export const ExerciseDetail = () => {
+  const [exerciseDetail, setExerciseDetail] = useState({})
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchExerciseData = async () => {
+
+    }
+    fetchExerciseData()
+
+  }, [id])
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail />
+      <ExerciseVideos />
+      <SimilarExercises />
+      ExerciseDetail</Box>
   )
 }
